e4ecf5c978b153216f3b3c3b1655586d80dd2136
Karl Greenhalgh <karldgreenhalgh@gmail.com>
2017-05-09 21:50:36 -0400

    Homing missile

---
+++ ProjectTD/Assets/_game/scripts/Projectiles/Missile.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Missile : MonoBehaviour
+{
+    public Transform Target;
+    public float flightSpeed = 2f;
+    float rotSpeed = 2.5f;
+    float lastRot= 0f;
+
+    private Rigidbody2D rb;
+    Quaternion newRot;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        Invoke("Explode", 5f);
+    }
+
+    void FixedUpdate()
+    {
+        newRot = Quaternion.LookRotation(transform.position - Target.position, Vector3.forward);
+        newRot.x = newRot.y = 0f;
+        transform.rotation = Quaternion.Slerp(transform.rotation, newRot, Time.deltaTime * rotSpeed);
+        rb.velocity = transform.up * flightSpeed;
+        if(rotSpeed < 40f)
+        {
+            lastRot += Time.deltaTime * Time.deltaTime * 50f;
+            rotSpeed += lastRot;
+        }
+    }
+
+    private void Explode()
+    {
+        CancelInvoke("Explode");
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Explode();
+        }
+    }
+}
---
+++ ProjectTD/Assets/_game/scripts/Towers/SimpleTower.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleTower : MonoBehaviour
+{
+    public Transform target;
+    public GameObject shotPrefab;
+    float shotTime = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shotTime += Time.deltaTime;
+        if (shotTime >= 0.75f)
+        {
+            shotTime = 0;
+            GameObject shot = (GameObject)Instantiate(shotPrefab, this.transform);
+            shot.transform.position = transform.position;
+            Missile missileScript = shot.GetComponent<Missile>();
+            missileScript.Target = this.target;
+        }
+    }
+}
